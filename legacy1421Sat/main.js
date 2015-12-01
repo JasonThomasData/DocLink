6495d571c4934afce17ed5e1900ad8215938d1f5
Jason Thomas <JasonThomasData@users.noreply.github.com>
2015-12-01 23:24:03 +1100

    Not needed any more

--- legacy1421Sat/main.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-	var languages = ['English', 'Arabic', 'Mandarin'];
-	for (i=0; i< languages.length; i++){
-		$('#chooseLanguage').append('<div>')
-		$('#chooseLanguage div:last-child').html(languages[i]).attr('class', 'language')
-	}
-});
