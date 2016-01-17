39523599a0302956e8b5ed0e75c0f552c393a7fe
Luke Bacon <luke@equivalentideas.com>
2016-01-17 23:37:13 +1100

    Fix formatting

--- spec/factories/doctors.rb
+++ spec/factories/doctors.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :doctor do
     name         "Doctor Jason"
-    address       "300 King St Newtown, NSW"
+    address      "300 King St, Newtown, NSW"
     phone_number "95169889"
   end
 end
