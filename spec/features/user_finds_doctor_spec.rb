a3ed31d2b3a3d11d1bcd7fcf741ee32e580c1ff6
Luke Bacon <luke@equivalentideas.com>
2016-02-25 20:03:02 +1100

    Use descriptive variables to make test clearer (refactor only)
    It isn't clear that we're making sure the far away doctor isn't shown.

--- spec/features/user_finds_doctor_spec.rb
+++ spec/features/user_finds_doctor_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 feature "User finds a doctor" do
   scenario "successfully" do
     VCR.use_cassette('doclink') do
-      create(
+      close_doctor = create(
         :doctor,
         name: "Doctor Jason",
         phone_number: "95169889",
         address: "300 King St, Newtown, NSW"
       )
 
-      create(
+      far_doctor = create(
         :doctor,
         name: "Doctor Fatima",
         phone_number: "0406563694",
@@ -21,10 +21,10 @@ feature "User finds a doctor" do
       fill_in "Find doctors near by", with: "Newtown NSW"
       click_button "Search"
 
-      expect(page).to have_content "Doctor Jason"
-      expect(page).to have_content "95169889"
-      expect(page).to_not have_content "Doctor Fatima"
-      expect(page).to_not have_content "0406563694"
+      expect(page).to have_content close_doctor.name
+      expect(page).to have_content close_doctor.phone_number
+      expect(page).to_not have_content far_doctor.name
+      expect(page).to_not have_content far_doctor.phone_number
     end
   end
 end
