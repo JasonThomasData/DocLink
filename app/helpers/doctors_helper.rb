7faf7f487beccca8566ad9330d51d8840ebf60d5
JasonThomasData <jason.robert.thomas.123@gmail.com>
2016-05-22 18:15:41 +1000

    Fixed geolocate issue, / removed from query string

--- app/helpers/doctors_helper.rb
+++ app/helpers/doctors_helper.rb
@@ -14,9 +14,20 @@ module DoctorsHelper
     address.sub(/, Australia$/,"").sub(/, NSW/,"")
   end
 
+  def remove_slash_from_address(address)
+    return address.gsub("/", ",")
+  end
+
+  def remove_space_from_address(address_no_slash)
+    return address_no_slash.gsub(" ", "+")
+  end
+
   def map_link(address)
+    address_no_slash = remove_slash_from_address(address)
+    address_no_spaces = remove_space_from_address(address_no_slash)
+
     link_to t("map_link.text"),
-            "https://www.google.com.au/maps/place/#{address.gsub(" ", "+")}/",
+            "https://www.google.com.au/maps/place/#{address_no_spaces}/",
             title: t("map_link.title"),
             class: "doctor-link-map"
   end
