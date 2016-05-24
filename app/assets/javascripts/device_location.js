c2786a0055014171703af2057c29dd1aaac894e6
JasonThomasData <jason.robert.thomas.123@gmail.com>
2016-05-24 22:04:57 +1000

    App now has a button on homepage that finds the device location

--- app/assets/javascripts/device_location.js
+++ app/assets/javascripts/device_location.js
@@ -1,43 +1,47 @@
-function showAddress(lat, lng){
-	alert(lat + ' ' + lng)
-}
+$(document).ready(function() {
 
-function formatAPIcall(lat, lng){
-	var googleAPI = 'https://maps.googleapis.com/maps/api/geocode/json?address='
-	var queryString = lat + ',' + lng
-	return geocodeURL = googleAPI + queryString
-	//EG - https://maps.googleapis.com/maps/api/geocode/json?address=-33.8078584,151.1799006
-}
+	function putAddressInSearchBox(formattedAddress) {
+		$('#q').val(formattedAddress) //So that the user can search for this. 
+	}
 
-function reverseGeocode(geocodeURL, callback){
-	$.get(geocodeURL, function(dataFromGoogle){
-		var formattedAddress = selectAddress(dataFromGoogle)
-		alert(formattedAddress)
-	})
-}
+	function formatAPIcall(lat, lng) {
+		var googleAPI = 'https://maps.googleapis.com/maps/api/geocode/json?address='
+		var queryString = lat + ',' + lng
+		return geocodeURL = googleAPI + queryString
+		//EG - https://maps.googleapis.com/maps/api/geocode/json?address=-33.8078584,151.1799006
+	}
 
-function selectAddress(dataFromGoogle){
-	return dataFromGoogle.results[0].formatted_address
-}
+	function reverseGeocode(geocodeURL, callback) {
+		$.get(geocodeURL, function(dataFromGoogle) {
+			var formattedAddress = selectAddress(dataFromGoogle)
+			putAddressInSearchBox(formattedAddress)
+		})
+	}
 
-function success(position) {
-    var lat = position.coords.latitude;
-    var lng = position.coords.longitude;
-    var geocodeURL = formatAPIcall(lat,lng)
-    showAddress(lat, lng)
-    reverseGeocode(geocodeURL)
-}
+	function selectAddress(dataFromGoogle) {
+		console.log(dataFromGoogle)
+		return dataFromGoogle.results[1].formatted_address
+	}
 
-function error() {
-    //return 'Error'
-}
+	function success(position) {
+	    var lat = position.coords.latitude;
+	    var lng = position.coords.longitude;
+	    var geocodeURL = formatAPIcall(lat,lng)
+	    reverseGeocode(geocodeURL)
+	}
 
-function getLatLng() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(success, error)
-    }
-}
+	function error() {
+	    alert("We are unable to find your address")
+	}
 
-$(document).ready(function(){
-	getLatLng();
+	function getLatLng() {
+	    if (navigator.geolocation) {
+	        navigator.geolocation.getCurrentPosition(success, error)
+	    }
+	}
+
+	$('#get_location').click(function(){
+		getLatLng();
+	});
+
 });
