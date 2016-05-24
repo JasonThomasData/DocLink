2022acd43df95b5822da03baf7b755f4b31ba8fb
JasonThomasData <jason.robert.thomas.123@gmail.com>
2016-05-24 22:12:58 +1000

    App now has a button on homepage that finds the device location

--- app/assets/javascripts/device_location.js
+++ app/assets/javascripts/device_location.js
@@ -4,23 +4,23 @@ $(document).ready(function() {
 		$('#q').val(formattedAddress) //So that the user can search for this. 
 	}
 
+	function getSuburbState(dataFromGoogle) {
+		console.log(dataFromGoogle)
+		return dataFromGoogle.results[1].formatted_address //The second location item is good. The first is too specific, and HTML geolcation is unreliable
+	}
+
+	function reverseGeocode(geocodeURL, callback) {
+		$.get(geocodeURL, function(dataFromGoogle) {
+			var formattedAddress = getSuburbState(dataFromGoogle)
+			putAddressInSearchBox(formattedAddress)
+		})
+	}
+
 	function formatAPIcall(lat, lng) {
 		var googleAPI = 'https://maps.googleapis.com/maps/api/geocode/json?address='
 		var queryString = lat + ',' + lng
 		return geocodeURL = googleAPI + queryString
 		//EG - https://maps.googleapis.com/maps/api/geocode/json?address=-33.8078584,151.1799006
-	}
-
-	function reverseGeocode(geocodeURL, callback) {
-		$.get(geocodeURL, function(dataFromGoogle) {
-			var formattedAddress = selectAddress(dataFromGoogle)
-			putAddressInSearchBox(formattedAddress)
-		})
-	}
-
-	function selectAddress(dataFromGoogle) {
-		console.log(dataFromGoogle)
-		return dataFromGoogle.results[1].formatted_address
 	}
 
 	function success(position) {
@@ -31,17 +31,17 @@ $(document).ready(function() {
 	}
 
 	function error() {
-	    alert("We are unable to find your address")
+	    alert("We are unable to find your address.")
 	}
 
-	function getLatLng() {
+	function findMySuburb() {
 	    if (navigator.geolocation) {
 	        navigator.geolocation.getCurrentPosition(success, error)
 	    }
 	}
 
 	$('#get_location').click(function(){
-		getLatLng();
+		findMySuburb();
 	});
 
 });
