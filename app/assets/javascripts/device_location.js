0a244c8f6ccade82b591425ffbfb67d0c6092190
Jason Thomas <JasonThomasData@users.noreply.github.com>
2016-05-28 10:20:31 +1000

    Changing JS to be two spaces indented
    Since we're using Ruby, it's nice to have similar indentation across the project.

--- app/assets/javascripts/device_location.js
+++ app/assets/javascripts/device_location.js
@@ -1,47 +1,47 @@
 $(document).ready(function() {
 
-	function putAddressInSearchBox(formattedAddress) {
-		$('#q').val(formattedAddress) //So that the user can search for this. 
-	}
+  function putAddressInSearchBox(formattedAddress) {
+    $('#q').val(formattedAddress) //So that the user can search for this. 
+  }
 
-	function getSuburbState(dataFromGoogle) {
-		console.log(dataFromGoogle)
-		return dataFromGoogle.results[1].formatted_address //The second location item is good. The first is too specific, and HTML geolcation is unreliable
-	}
+  function getSuburbState(dataFromGoogle) {
+    console.log(dataFromGoogle)
+    return dataFromGoogle.results[1].formatted_address //The second location item is good. The first is too specific, and HTML geolcation is unreliable
+  }
 
-	function reverseGeocode(geocodeURL, callback) {
-		$.get(geocodeURL, function(dataFromGoogle) {
-			var formattedAddress = getSuburbState(dataFromGoogle)
-			putAddressInSearchBox(formattedAddress)
-		})
-	}
+  function reverseGeocode(geocodeURL, callback) {
+    $.get(geocodeURL, function(dataFromGoogle) {
+      var formattedAddress = getSuburbState(dataFromGoogle)
+      putAddressInSearchBox(formattedAddress)
+    })
+  }
 
-	function formatAPIcall(lat, lng) {
-		var googleAPI = 'https://maps.googleapis.com/maps/api/geocode/json?address='
-		var queryString = lat + ',' + lng
-		return geocodeURL = googleAPI + queryString
-		//EG - https://maps.googleapis.com/maps/api/geocode/json?address=-33.8078584,151.1799006
-	}
+  function formatAPIcall(lat, lng) {
+    var googleAPI = 'https://maps.googleapis.com/maps/api/geocode/json?address='
+    var queryString = lat + ',' + lng
+    return geocodeURL = googleAPI + queryString
+    //EG - https://maps.googleapis.com/maps/api/geocode/json?address=-33.8078584,151.1799006
+  }
 
-	function success(position) {
-	    var lat = position.coords.latitude;
-	    var lng = position.coords.longitude;
-	    var geocodeURL = formatAPIcall(lat,lng)
-	    reverseGeocode(geocodeURL)
-	}
+  function success(position) {
+    var lat = position.coords.latitude;
+    var lng = position.coords.longitude;
+    var geocodeURL = formatAPIcall(lat,lng)
+    reverseGeocode(geocodeURL)
+  }
 
-	function error() {
-	    alert("We are unable to find your address.")
-	}
+  function error() {
+    alert("We are unable to find your address.")
+  }
 
-	function findDeviceLocation() {
-	    if (navigator.geolocation) {
-	        navigator.geolocation.getCurrentPosition(success, error)
-	    }
-	}
+  function findDeviceLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(success, error)
+    }
+  }
 
-	$('#get_location').click(function(){
-		findDeviceLocation();
-	});
+  $('#get_location').click(function(){
+    findDeviceLocation();
+  });
 
 });
